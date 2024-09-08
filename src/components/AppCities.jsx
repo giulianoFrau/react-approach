e5532babfb3d44df0b25090472d0fdd3db0e5b98
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-08 18:38:51 +0200

    refact code

--- src/components/AppCities.jsx
+++ src/components/AppCities.jsx
@@ -1,28 +1,25 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../components/AppCities.scss";
 import { Cities } from "../api/index.js";
-import { InputText } from 'primereact/inputtext';
-        
-        
+import { InputText } from "primereact/inputtext";
 
 const AppCites = () => {
-
   const [AllCities, setAllCities] = useState([]); //ref dove salvo result api città
 
   const [cityName, setCityName] = useState(""); //ref per input
 
-  const [filteredCities, setFilteredCities] = useState([]);  //ref dove salvo i risultati filtrati
-
+  const [filteredCities, setFilteredCities] = useState([]); //ref dove salvo i risultati filtrati
 
   let inputHandler = (e) => {
     let lowerCase = e.target.value.toLowerCase();
     setCityName(lowerCase);
   };
 
-
   async function getCities() {
     const resp = await Cities.getCities();
-    setAllCities(resp.data.sort((a, b) => (a.name.common > b.name.common ? 1 : -1)));
+    setAllCities(
+      resp.data.sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
+    );
   }
 
   useEffect(() => {
@@ -38,25 +35,26 @@ const AppCites = () => {
 
   return (
     <>
-    <div className="app__cities flex flex-col gap-7">
-    <InputText
-       className="border"
-        onChange={inputHandler} 
-        value={cityName}>
-      </InputText>
-     
-      <div className="app__cities__container flex flex-wrap gap-5 justify-center">
-        { filteredCities.length >0 ?
-        filteredCities.map((item, index) => (
-          <div key={index} className="app__cities__container-card p-3 border rounded-md">
-            
-            <div>{item.name.common}</div></div>
-        )) : "Nessun Risultato"}
+      <div className="app__cities flex flex-col gap-7">
+        <InputText
+          className="border text-indigo-500 p-2"
+          onChange={inputHandler}
+          value={cityName}
+        ></InputText>
+
+        <div className="app__cities__container flex flex-wrap gap-5 justify-center">
+          {filteredCities.length > 0
+            ? filteredCities.map((item, index) => (
+                <div
+                  key={index}
+                  className="app__cities__container-card p-3 border rounded-md"
+                >
+                  <div>{item.name.common}</div>
+                </div>
+              ))
+            : "Nessun Risultato"}
+        </div>
       </div>
-    </div>
-    
-     
-      
     </>
   );
 };
