e64b0ea0b12eecc0163be0be64b18429717a36f3
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-09 12:09:38 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card.jsx";
+
 function App() {
   /* reattività: creo le variabili e li suo metodo. dopo di che se la voglio chiamare al click la chiamo in altro modo,
   e dentro la funzione chiamo la setCount */
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   function funzione() {
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
   return (
     <>
-      <Navbar/>
+      <div className="grid frid-cols-4 gap-10">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,9 +31,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={funzione}>
-          count is {count}
-        </button>
+        <button onClick={funzione}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -35,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
