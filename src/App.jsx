aba6e995e9ad6ef81bc48d3c6f2298478d080753
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-09 13:05:42 +0200

    feat: loop e conditional rending

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
 import Cards from "./components/Cards";
 import "./App.css";
 
+const cities = [
+  {
+    title: "Roma",
+    isVisited: true,
+    imgUrl:
+      "https://media.istockphoto.com/id/509475506/it/foto/roman-citscape-panorama-al-tramonto-roma-italia.jpg?s=1024x1024&w=is&k=20&c=MTO5DpZiJrbDhNfGRdBJIq9nU0WV-Hqk13ojlMJbGwY=",
+    description:
+      "1Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam  tempore in, quo consequatur voluptatum error necessitatibus, harum numquam tenetur omnis odio. Error ea earum voluptate praesentium.",
+  },
+
+  {
+    title: "Milano",
+    isVisited: false,
+    imgUrl:
+      "https://media.istockphoto.com/id/687224330/it/foto/piazza-del-duomo-un-duomo-milano-allalba.jpg?s=1024x1024&w=is&k=20&c=SF9tMNGWcA6CEdPXnxz1zrE0eE-IBPTQCKZmNM6nMHw=",
+    description:
+      "2Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam  tempore in, quo consequatur voluptatum error necessitatibus, harum numquam tenetur omnis odio. Error ea earum voluptate praesentium.",
+  },
+
+  {
+    title: "Napoli",
+    isVisited: false,
+    imgUrl:
+      "https://media.istockphoto.com/id/1097360124/it/foto/veduta-aerea-di-napoli-italia.jpg?s=1024x1024&w=is&k=20&c=4m9nKc8TTID0MK7q6JlfQniQz2Zs-NbxbEllr2s7FiU=",
+    description:
+      "3Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam  tempore in, quo consequatur voluptatum error necessitatibus, harum numquam tenetur omnis odio. Error ea earum voluptate praesentium.",
+  },
+
+  {
+    title: "Cagliari",
+    isVisited: true,
+    imgUrl:
+      "4https://media.istockphoto.com/id/528438369/it/foto/cagliari-italia-paesaggio-urbano.jpg?s=1024x1024&w=is&k=20&c=hvLNnBtUJsBE3Wr0OpAaOkEdotnMekOPiiUld0czI5s=",
+    description:
+      " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam tempore in, quo consequatur voluptatum error necessitatibus, harum numquam tenetur omnis odio. Error ea earum voluptate praesentium.",
+  },
+];
+
 function App() {
   return (
     <div className="grid grid-cols-4 gap-5">
-      <Cards
-        title="Roma"
-        imgUrl="https://media.istockphoto.com/id/509475506/it/foto/roman-citscape-panorama-al-tramonto-roma-italia.jpg?s=1024x1024&w=is&k=20&c=MTO5DpZiJrbDhNfGRdBJIq9nU0WV-Hqk13ojlMJbGwY="
-      >
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
-        tempore in, quo consequatur voluptatum error necessitatibus, harum
-        numquam tenetur omnis odio. Error ea earum voluptate praesentium.
-        Molestias quae odit explicabo!{" "}
-        {/* questo è un children, messo dentro Cards, da mettere come oggetto di parametro della funzione*/}
-      </Cards>
-      <Cards
-        title="Milano"
-        imgUrl="https://media.istockphoto.com/id/687224330/it/foto/piazza-del-duomo-un-duomo-milano-allalba.jpg?s=1024x1024&w=is&k=20&c=SF9tMNGWcA6CEdPXnxz1zrE0eE-IBPTQCKZmNM6nMHw="
-      />
-      <Cards
-        title="Napoli"
-        imgUrl="https://media.istockphoto.com/id/1097360124/it/foto/veduta-aerea-di-napoli-italia.jpg?s=1024x1024&w=is&k=20&c=4m9nKc8TTID0MK7q6JlfQniQz2Zs-NbxbEllr2s7FiU="
-      />
-      <Cards
-        title="Cagliari"
-        imgUrl="https://media.istockphoto.com/id/528438369/it/foto/cagliari-italia-paesaggio-urbano.jpg?s=1024x1024&w=is&k=20&c=hvLNnBtUJsBE3Wr0OpAaOkEdotnMekOPiiUld0czI5s="
-      />
+      {cities.map((city, i) => (
+        <Cards
+          key={i}
+          title={city.title}
+          imgUrl={city.imgUrl}
+          isVisited={city.isVisited}
+        >
+          {city.description}
+        </Cards>
+      ))}
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,11 @@
-const Cards = ({ imgUrl, title, children }) => {
+const Cards = ({ imgUrl, title, children, isVisited }) => {
   return (
     <div className="rounded-md bg-zinc-950 ">
       <img src={imgUrl} />
       <div className="flex flex-col p-4">
         <h2 className="text-2xl">{title}</h2>
         <p className="text-gray-500">{children}</p>
+        <span>{isVisited ? "Città visitata" : "Città non visitata"}</span>
       </div>
     </div>
   );
