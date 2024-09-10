7e42a2dde0d48712c761d7cabab6b0e29ec6ee25
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-10 18:24:59 +0200

    feaT: modifica bandiere

--- src/components/PreferCities.jsx
+++ src/components/PreferCities.jsx
@@ -3,11 +3,13 @@ import { useSelector } from "react-redux";
 const PreferCities = ({ allCities }) => {
   const preferCities = useSelector((state) => state.cities.preferCities);
 
+  // Crea una mappa per l'accesso rapido ai dati delle città
   const citiesMap = allCities.reduce((acc, city) => {
     acc[city.name.common.toLowerCase()] = city;
     return acc;
   }, {});
 
+  // Raggruppa preferCities per lettera iniziale
   const groupedCities = preferCities.reduce((acc, cityName) => {
     const cityInfo = citiesMap[cityName.toLowerCase()];
     if (cityInfo) {
@@ -18,28 +20,31 @@ const PreferCities = ({ allCities }) => {
     return acc;
   }, {});
 
+  // Ordina le città all'interno di ciascun gruppo per lettera iniziale
+  Object.keys(groupedCities).forEach((letter) => {
+    groupedCities[letter].sort((a, b) =>
+      a.name.common.localeCompare(b.name.common)
+    );
+  });
+
   return (
     <div className="grid grid-cols-3 gap-5">
       {Object.keys(groupedCities)
-        .sort()
+        .sort() // Ordina le lettere iniziali
         .map((letter) => (
           <div key={letter}>
             <div className="flex flex-col gap-3 text-3xl mb-3">{letter}</div>
             {groupedCities[letter].map((city) => (
-              <div
-                key={city.name.common}
-                className="flex gap-2 align-items-center justify-content-center"
-              >
-                <div className="mb-3">
+              <div key={city.name.common} className="flex gap-2 mb-3">
+                <div>
                   {city.flags.png && (
                     <img
                       src={city.flags.png}
                       style={{ width: 30, height: 20 }}
-                      alt={`Flag  ${city.name.common}`}
+                      alt={`Flag of ${city.name.common}`}
                     />
                   )}
                 </div>
-
                 <div>{city.name.common}</div>
               </div>
             ))}
