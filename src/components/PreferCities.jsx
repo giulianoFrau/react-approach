8b669e866196ea7cbad2b8d5f6e4e0d3dee43a87
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-10 18:21:06 +0200

    refact

--- src/components/PreferCities.jsx
+++ src/components/PreferCities.jsx
@@ -3,51 +3,49 @@ import { useSelector } from "react-redux";
 const PreferCities = ({ allCities }) => {
   const preferCities = useSelector((state) => state.cities.preferCities);
 
-  // Crea un oggetto per un accesso rapido ai dati delle città
   const citiesMap = allCities.reduce((acc, city) => {
     acc[city.name.common.toLowerCase()] = city;
     return acc;
   }, {});
 
-  // Raggruppa preferCities per lettera iniziale
-  const groupedCities = preferCities.reduce((acc, city) => {
-    const cityLower = city.toLowerCase();
-    const cityInfo = citiesMap[cityLower];
+  const groupedCities = preferCities.reduce((acc, cityName) => {
+    const cityInfo = citiesMap[cityName.toLowerCase()];
     if (cityInfo) {
       const firstLetter = cityInfo.name.common.charAt(0).toUpperCase();
-      if (!acc[firstLetter]) {
-        acc[firstLetter] = [];
-      }
+      if (!acc[firstLetter]) acc[firstLetter] = [];
       acc[firstLetter].push(cityInfo);
     }
     return acc;
   }, {});
 
   return (
-    <>
-      <div className="grid grid-cols-3 gap-5">
-        {Object.keys(groupedCities)
-          .sort()
-          .map((letter) => (
-            <div key={letter}>
-              <div className="text-3xl mb-3">{letter}</div>
-              {groupedCities[letter].map((city, i) => (
-                <div key={i}>
+    <div className="grid grid-cols-3 gap-5">
+      {Object.keys(groupedCities)
+        .sort()
+        .map((letter) => (
+          <div key={letter}>
+            <div className="flex flex-col gap-3 text-3xl mb-3">{letter}</div>
+            {groupedCities[letter].map((city) => (
+              <div
+                key={city.name.common}
+                className="flex gap-2 align-items-center justify-content-center"
+              >
+                <div className="mb-3">
                   {city.flags.png && (
                     <img
                       src={city.flags.png}
-                      alt={`Flag of ${city.name.common}`}
-                      style={{ width: 50, height: 30 }}
+                      style={{ width: 30, height: 20 }}
+                      alt={`Flag  ${city.name.common}`}
                     />
                   )}
-                  <div>{city.name.common}</div>
-                  <hr />
                 </div>
-              ))}
-            </div>
-          ))}
-      </div>
-    </>
+
+                <div>{city.name.common}</div>
+              </div>
+            ))}
+          </div>
+        ))}
+    </div>
   );
 };
 
