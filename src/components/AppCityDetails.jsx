3616cc6b62757368eff78e3b912b396eb45ca468
giulianoFrau <shokteo@gmail.com>
2024-09-15 22:07:08 +0200

    feat: aggiunto tasto home

--- src/components/AppCityDetails.jsx
+++ src/components/AppCityDetails.jsx
@@ -1,6 +1,7 @@
 
 
 import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 const AppCityDetails = () => {
   const location = useLocation();
   const { city } = location.state || {};
@@ -8,16 +9,20 @@ const AppCityDetails = () => {
     <div className="app__details sm:w-1/2 m-auto my-10 p-6 bg-white border border-gray-200 shadow-lg rounded-lg text-center text-xl ">
       <div className="text-3xl font-semibold text-gray-800 mb-2"> {city.name.common}</div>
       {city.flags? 
-      <div className="flex flex-col justify-center gap-2">
+      <div className="flex flex-col justify-center gap-2 mb-5 items-center">
 
-          <img src={city.flags.png} alt={city.name.common} className="sm:w-1/2 mx-auto"/>
+          <img src={city.flags.png} alt={city.name.common} className="sm:w-1/4 mx-auto"/>
           <div><b>Regione:</b> {city.region}</div>  
           <div><b>Capitale:</b> {city.capital[0]}</div>
           <div><b>Popolazione:</b> {city.population}</div>
           <div><b>Regione Appartenenza:</b> {city.subregion}</div>
           <div><b>Stato indipendente:</b> {city.independent==true?'Si':'No'}</div>
           <div><b>Lato guida</b> {city.car.side=='left'?'Sinistro':'Destro'}</div>
-      </div>:<div>Nessuna informazione disponibile per lo stato {city.name.common}</div>}
+      </div>:<div className="mb-5">Nessuna informazione disponibile per lo stato {city.name.common}</div>}
+      <Link className="underline rounded-md p-2 " to="/">
+      Torna alla home
+      </Link>
+
     </div>
   )
 }
